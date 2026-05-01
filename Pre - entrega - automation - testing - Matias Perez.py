f84e3153a572f60cabd1a7d20cad025b7d6fbff3
Matiasp013 <matias.perez_1991@hotmail.com>
2026-05-01 14:25:28 -0300

    Mis cambios antes de pull

---
+++ Pre - entrega - automation - testing - Matias Perez.py
@@ -0,0 +1,27 @@
+#ENTRADA / SALIDA DE DATOS
+
+# Dato => Cualquier cosa que escribo
+
+#TIPO DE DATOS 
+
+#STR
+#BOOL
+#INT FLOAT
+
+# Variables y asignacion
+
+#name = "Matias"
+
+#rint (*range(2,22,2))
+
+nota = int(input("Ingresa una nota : "))
+
+if nota >= 9:
+    print("Excelente")
+
+elif nota >= 6: 
+    print("Aprobado")
+else: 
+    print("Desaprobaste")
+
+
